aa6343f3c8e85d3598845884e7aa1f3b16ef81ce
ReddyVanya <92796326+ReddyVanya@users.noreply.github.com>
2021-10-30 08:33:36 +0530

    Time: 1 ms (99.86%), Space: 39.6 MB (74.61%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] list = new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+        {
+            list[i] = nums[nums[i]];
+        }
+        return list;
+    }
+}
